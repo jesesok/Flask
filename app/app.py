cffa2bc7dd07a97718790e294569661b78b0c7e4
Никита Моисеев <jeses47@gmail.com>
2021-04-11 17:30:34 +0300

    added color

--- app/app.py
+++ app/app.py
@@ -2,7 +2,17 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql///newdb'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///new.db'
+# db = SQLAlchemy(app)
+
+
+# class Page(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(500), nullable=False)
+#     body = db.Column(db.String(500), nullable=False)
+#
+#     def __repr__(self):
+#         return '<Article %r>' % self.id
 
 
 @app.route('/')
